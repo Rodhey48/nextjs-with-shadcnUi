8bbe1d735ce76ce94e20f905dd84ac6ed6ad3ed1
Rudi <rudi@wikarealty.co.id>
2024-01-04 08:23:23 +0700

    initial

---
+++ src/pages/abouts.tsx
@@ -0,0 +1,6 @@
+export default function About() {
+    return (<p>
+        INI ABOUTS
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, dolorum quaerat odit veritatis magnam itaque officia ea labore cupiditate necessitatibus. Ex eos nisi vel voluptatum adipisci dicta? Deserunt, aliquam placeat.
+    </p>)
+}
